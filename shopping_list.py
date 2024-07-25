7d7f93d83521e9c222efae1bbadbce0552a7a5ef
Piotr Pięciak <piotr.pieciak@gmail.com>
2024-07-25 15:51:03 +0200

    Add file shopping_list.py

---
+++ shopping_list.py
@@ -0,0 +1,13 @@
+purch_list = {
+  "piekarnia": ["chleb", "bułki", "pączek"],
+  "warzywniak": ["marchew", "seler", "rukola"]
+}
+print("Lista zakupów")
+
+prod_number=0
+for sklep, produkty in purch_list.items():
+  capit_produkty = [p.upper() for p in produkty]
+  prod_number= prod_number + len(produkty)
+  print(f"Idę do {sklep.upper()} i kupuję tam {capit_produkty}.")
+
+print(f"W sumie kupuję {prod_number} produktów.")
